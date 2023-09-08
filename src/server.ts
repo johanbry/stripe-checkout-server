ca08a9edc0c9f09cffec0a686624b9dfc15ce232
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-08 15:24:50 +0200

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import userRouter from "./routes/userRouter";
 
@@ -7,6 +8,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
