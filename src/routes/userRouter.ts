4a03a7b272872b9f19fc70d403432ddb0e608172
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-19 19:06:48 +0200

    endpoint for my orders and added middleware

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -5,12 +5,16 @@ import {
   logoutUser,
   authorizeUser,
   registerUser,
+  getLoggedInOrders,
 } from "../controllers/userController";
+import { auth, validate } from "../middleware/middleware";
+import { registerSchema } from "../schemas/userSchema";
 
 const userRouter = Router()
-  .post("/register", registerUser)
+  .post("/register", validate(registerSchema), registerUser)
   .post("/login", loginUser)
   .get("/logout", logoutUser)
-  .get("/authorize", authorizeUser);
+  .get("/authorize", authorizeUser)
+  .get("/me/orders", auth, getLoggedInOrders);
 
 export default userRouter;
