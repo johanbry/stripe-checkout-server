c2043585972be5087ea7ca374e73469e015c5841
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-19 20:38:28 +0200

    added routers

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import cookieSession from "cookie-session";
 
 import userRouter from "./routes/userRouter";
 import productRouter from "./routes/productRouter";
+import checkoutRouter from "./routes/checkoutRouter";
+import orderRouter from "./routes/orderRouter";
 
 dotenv.config();
 
@@ -32,6 +34,8 @@ app.use(express.json());
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
+app.use("/api/checkout", checkoutRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(port, () => {
   console.log(`Server is up and listening on port: ${port}`);
