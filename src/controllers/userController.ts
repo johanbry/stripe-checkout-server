2e5a2269ab27cb0dde83b21b8b12cea2ae1e607f
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-19 20:36:36 +0200

    refactored and added controller func userorders

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,30 +1,21 @@
-import express, { Request, Response } from "express";
-import fs from "fs/promises";
+import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import path from "path";
 
 import initStripe from "../stripe/stripe";
+import { IServerUser, IClientUser, IOrder } from "../interfaces/interfaces";
+import { getOrdersByUser } from "../services/orderService";
+import { readUsers, saveUsers } from "../db/userDb";
 
-const usersFile = "data/users.json";
-
-interface IClientUser {
-  stripeId: string;
-  name: string;
-  email: string;
-}
-interface IServerUser extends IClientUser {
-  password: string;
-}
+const USERS_FILE = "data/users.json";
 
 export const registerUser = async (req: Request, res: Response) => {
   const { firstName, lastName, email, password } = req.body;
   let newUser: IServerUser | null = null;
   let users: IServerUser[] = [];
 
-  // Read json-file and save current users to array. Handle error when no data file exists.
+  // Get all users
   try {
-    const fileBuffer = await fs.readFile(usersFile);
-    if (fileBuffer.length > 1) users = JSON.parse(fileBuffer.toString());
+    users = (await readUsers()) || [];
   } catch (error) {
     console.log("Error open/read file.", error);
   }
@@ -55,7 +46,7 @@ export const registerUser = async (req: Request, res: Response) => {
       };
 
       users.push(newUser);
-      await fs.writeFile(usersFile, JSON.stringify(users, null, 2));
+      saveUsers(users);
 
       // Return the new user with the response, but without the password.
       const clientUser: IClientUser = {
@@ -76,9 +67,9 @@ export const loginUser = async (req: Request, res: Response) => {
   let users: IServerUser[] = [];
 
   // Read json-file and save current users to array. Handle error when no data file exists.
+  // Get all users
   try {
-    const fileBuffer = await fs.readFile(usersFile);
-    if (fileBuffer.length > 1) users = JSON.parse(fileBuffer.toString());
+    users = (await readUsers()) || [];
   } catch (error) {
     return res.status(500).json("Error logging in, please try again. " + error);
   }
@@ -114,4 +105,15 @@ export const authorizeUser = (req: Request, res: Response) => {
   if (!req.session?.stripeId) res.status(401).json("Not logged in.");
   // Return the user object in the cookie as response data.
   else res.status(200).json(req.session);
+};
+
+export const getLoggedInOrders = async (req: Request, res: Response) => {
+  const userId = req.session?.stripeId;
+  if (!userId)
+    return res.status(400).json("Must be logged in for this request.");
+
+  let orders: IOrder[] | null = [];
+  orders = await getOrdersByUser(userId);
+
+  res.status(200).json(orders);
 };
