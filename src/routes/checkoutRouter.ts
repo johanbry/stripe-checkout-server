ec3e792af58015b601df4ec7c4c4b3be8df264f2
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-09-22 17:35:42 +0200

    added auth middleware to endpoint

--- src/routes/checkoutRouter.ts
+++ src/routes/checkoutRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { createStripeCheckoutSession } from "../controllers/checkoutController";
+import { auth } from "../middleware/middleware";
 
 const checkoutRouter = Router().post(
   "/create-stripe-session",
+  auth,
   createStripeCheckoutSession
 );
 
